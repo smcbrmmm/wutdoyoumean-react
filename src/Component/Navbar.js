345dc0f1602ff4abd998752e5a76690ada984005
Samut Chouybumrung <samut.c@ku.th>
2020-11-29 20:05:08 +0700

    first commit

---
+++ src/Component/Navbar.js
@@ -0,0 +1,11 @@
+import { Button } from 'react-bootstrap';
+
+function Navbar() {
+    return (
+      <div className="App">
+          <Button variant="primary" href="/about" >Primary</Button>{' '}
+      </div>
+    );
+  }
+  
+  export default Navbar;
